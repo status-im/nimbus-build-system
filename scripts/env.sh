9f43764458f1a98ceceb40bb00a8066d3e8f5eaa
Zahary Karadjov <zahary@gmail.com>
2020-02-24 16:41:32 +0200

    Add the Nimble bin directory to the PATH
    I'm using this directory as a place to put the new testrunner exe
    from the testutils package. For now, I'm doing this manually, but
    it may be automated by the Makefiles in the future.

--- scripts/env.sh
+++ scripts/env.sh
@@ -27,12 +27,12 @@ export GO111MODULE=on
 # looks like oh-my-zsh can't handle dots in PATH
 export NIM_PATH=$(cd "${ABS_PATH}/../vendor/Nim/bin"; pwd)
 
-#- make it an absolute path, so we can call this script from other dirs
-#- we can't use native Windows paths in here, because colons can't be escaped in PATH
-export PATH="${NIM_PATH}:${GOPATH}/bin:${PATH}"
-
 # Nimble needs this to be an absolute path
 export NIMBLE_DIR="${ABS_PATH}/../../.nimble"
+
+#- make it an absolute path, so we can call this script from other dirs
+#- we can't use native Windows paths in here, because colons can't be escaped in PATH
+export PATH="${NIM_PATH}:${NIMBLE_DIR}/bin:${GOPATH}/bin:${PATH}"
 
 # used by nim-beacon-chain/tests/simulation/start.sh
 export BUILD_OUTPUTS_DIR="${ABS_PATH}/../../../build"
