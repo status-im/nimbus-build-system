9bad27f6c2b53133396dc454282d341ea9cd1f7b
Ștefan Talpalaru <stefantalpalaru@yahoo.com>
2020-07-16 21:02:06 +0200

    show-deps: also show commit hashes

---
+++ scripts/git_show_deps.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -e
+
+if [[ -f .gitmodules ]]; then
+  git config --file .gitmodules --get-regexp 'path|url' | while read TMP S_PATH && read TMP S_URL; do
+    # we probably can't rely on that leading space always being there
+    S_HASH=$(git submodule status --cached "${S_PATH}" | sed 's/^\s*\(\S\+\).*$/\1/')
+    echo "${S_URL} ${S_HASH}"
+  done
+fi
+
