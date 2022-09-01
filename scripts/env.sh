e883a9e91f5ff4ec91f83d1baa49542727c30d9a
Ivan Yonchovski <yyoncho@gmail.com>
2022-09-01 11:32:12 +0300

    Make sure the path is understandable for nim on windows

--- scripts/env.sh
+++ scripts/env.sh
@@ -70,6 +70,9 @@ export NIMBUS_BUILD_SYSTEM=yes
 echo "--noNimblePath" > nimbus-build-system.paths
 for file in $(ls -d $PWD/vendor/*)
 do
+  if uname | grep -qiE "mingw|msys"; then
+    file=$(cygpath -m $file)
+  fi
   if [ -d "$file/src" ]; then
     echo --path:"\"$file/src\""
   else
