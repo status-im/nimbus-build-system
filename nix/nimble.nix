67f395831f94ba4ac5fb4c511beb99bc6ec63e22
Jakub Sokołowski <jakub@status.im>
2026-03-27 10:49:08 +0100

    nix: fix nimble package name and hash
    A typo in package name caused rev to not be included and hash to not be
    refreshed during builds on machines with cached source.
    
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

--- nix/nimble.nix
+++ nix/nimble.nix
@@ -7,12 +7,12 @@ let
   nbsVersion = findKeyValue "^[[:space:]]+NIMBLE_COMMIT='([a-f0-9]+)'.*$" ../scripts/build_nim.sh;
   nimVersion = findKeyValue "^[[:space:]]+NimbleStableCommit = \"([a-f0-9]+)\".*$" ../vendor/Nim/koch.nim;
 in pkgs.fetchFromGitHub rec {
-  name = "${owner}-${repo}-src{rev}";
+  name = "${owner}-${repo}-src-${rev}";
   owner = "nim-lang";
   repo = "nimble";
   fetchSubmodules = true;
   # Use Nimbsle verson defined in NBS or default to Nim one.
   rev = if nbsVersion != null then nbsVersion else nimVersion;
   # WARNING: Requires manual updates when Nim compiler version changes.
-  hash = "sha256-DV/cheAoG0UviYEYqfaonhrAl4MgjDwFqbbKx7jUnKE=";
+  hash = "sha256-v0RhIx6ithFJqH6ThKpyvC0JB3CBCevahhCossC+deA";
 }
